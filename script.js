df17b75feb291110148a02653b7b28c2238b11fd
amiller0806 <ariel.miller555@gmail.com>
2022-08-31 16:42:30 -0400

    created validation for user preferencs of types of characters

--- script.js
+++ script.js
@@ -18,6 +18,17 @@ function generatePassword() {
   if (passwordLength  < 8 || passwordLength > 128 || isNaN(passwordLength)) {
     return "The password must be between 8 and 128 characters long and must be a number.";
   }
+var acceptUppercase = confirm("Do you want to use Uppercase letters?")
+var acceptLowercase = confirm("Do you want to use Lowercase letters?")
+var acceptNumbers = confirm("Do you want to use Numbers?")
+var acceptSpecial = confirm("Do you want to use special characters?")
+
+var userChoice = []
+if(!acceptUppercase || !acceptLowercase || !acceptNumbers || !acceptSpecial){
+  return "Please choose at least one of the following"
+}
+
+
 }
 
 // Add event listener to generate button
