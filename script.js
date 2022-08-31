8ce03c5f6939862a2e568dcad8f2003eaf5b33bb
amiller0806 <ariel.miller555@gmail.com>
2022-08-31 16:52:01 -0400

    Created different user choices

--- script.js
+++ script.js
@@ -15,21 +15,39 @@ function generatePassword() {
   var passwordLength = parseInt(prompt("Enter Password Length"))
 
 
-  if (passwordLength  < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+  if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
     return "The password must be between 8 and 128 characters long and must be a number.";
   }
-var acceptUppercase = confirm("Do you want to use Uppercase letters?")
-var acceptLowercase = confirm("Do you want to use Lowercase letters?")
-var acceptNumbers = confirm("Do you want to use Numbers?")
-var acceptSpecial = confirm("Do you want to use special characters?")
+  var acceptUppercase = confirm("Do you want to use Uppercase letters?")
+  var acceptLowercase = confirm("Do you want to use Lowercase letters?")
+  var acceptNumbers = confirm("Do you want to use Numbers?")
+  var acceptSpecial = confirm("Do you want to use special characters?")
 
-var userChoice = []
-if(!acceptUppercase || !acceptLowercase || !acceptNumbers || !acceptSpecial){
-  return "Please choose at least one of the following"
-}
+  var userChoice = []
+  if (!acceptUppercase || !acceptLowercase || !acceptNumbers || !acceptSpecial) {
+    return "Please choose at least one of the following"
+  }
 
+  if (acceptUppercase) {
+    userChoice += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+  if (acceptLowercase) {
+    userChoice += "abcdefghijklmnopqrstuvwxyz";
+  }
+
+  if (acceptNumbers) {
+    userChoice += "1234567890";
+  }
+
+  if (acceptSpecial) {
+    userChoice += "!@#$%^&*()*+,-./";
+  }
+
+  console.log(userChoice)
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
